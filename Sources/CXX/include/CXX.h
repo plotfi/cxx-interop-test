85f28e1715fe49fab6963e72ed0ea891ef11276b
zoecarver <z.zoelec2@gmail.com>
2021-03-08 14:44:53 -0800

    Add a simple program that uses std::string_view.

--- Sources/CXX/include/CXX.h
+++ Sources/CXX/include/CXX.h
@@ -1,4 +1,12 @@
 #ifndef CXX_H
 #define CXX_H
+
+#include <string_view>
+
 int cxx_f(int);
+
+bool is_str_42(std::string_view view) {
+    return view == "42";
+}
+
 #endif
--- Sources/Swift/main.swift
+++ Sources/Swift/main.swift
@@ -1,1 +1,14 @@
 import CXX
+import std.string_view
+
+"42".withCString { cstring in
+    let view = std.__1.string_view(cstring)
+    assert(is_str_42(view))
+}
+
+"32".withCString { cstring in
+    let view = std.__1.string_view(cstring)
+    assert(!is_str_42(view))
+}
+
+print("Done.")
