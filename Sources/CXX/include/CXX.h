f93b17ace0863aaf69bb957089cb3fcdb5dc8bad
Puyan Lotfi <puyan@puyan.org>
2021-03-25 23:38:36 -0400

    Revert "Some FB Vendor Headers extracted from ComponentKit"
    This reverts commit d5edd16e9c5e81dd3e935785af148dcd0796a232.

--- Sources/CXX/include/CXX.h
+++ Sources/CXX/include/CXX.h
@@ -1,7 +1,6 @@
 #ifndef CXX_H
 #define CXX_H
 
-#ifndef __ONLY_VENDOR__
 #include <string_view>
 
 int cxx_f(int);
@@ -9,14 +8,5 @@ int cxx_f(int);
 bool is_str_42(std::string_view view) {
     return view == "42";
 }
-#else
-
-#ifdef __VENDOR_HEADERS__
-// Include some headers that are Almost ObjC++.
-// Most are extracted from ComponentKit or other ObjC++ frameworks.
-#include "VendorHeaders/FB/CKCoalescedSpecSupport.h"
-#include "VendorHeaders/FB/CKWritingDirection.h"
-#endif
-#endif
 
 #endif
