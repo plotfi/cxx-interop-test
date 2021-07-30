f7c639903bd708b89b633387dee026751317bfb8
Puyan Lotfi <puyan@puyan.org>
2021-07-30 19:49:20 -0400

    Dropping some template specializations due to changes to _LIBCPP_HIDE_FROM_ABI
    https://github.com/llvm/llvm-project/commit/340b72520292d649fdbb0d5bcf78575870f302fc

--- Sources/CXX/CXX.cpp
+++ Sources/CXX/CXX.cpp
@@ -2,7 +2,5 @@
 // TODO: Generate these for specializing COMDATs during irgen.
 #include <vector>
 template class std::vector<int>;
-template class std::__vector_base<int, std::allocator<int>>;
-template class std::__vector_base_common<true>;
 
 int cxx_f(int a) { return 42 + a; }
