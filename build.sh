d62ea977d16b5d78c40d23536a3c152d749d204f
Puyan Lotfi <puyan@puyan.org>
2021-07-30 17:34:09 -0400

    Adding build.sh and CMakeLists.txt because SPM is flaking.

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-c++ -D_LIBCPP_HIDE_FROM_ABI="__attribute__((__visibility(\"hidden\")))" -c -std=c++11 ./Sources/CXX/CXX.cpp 
-swiftc -sdk /Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/ \
-  -I/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/c++/v1 \
+clang++ -D_LIBCPP_HIDE_FROM_ABI="__attribute__((__visibility(\"hidden\")))" -c -std=c++11 ./Sources/CXX/CXX.cpp
+swiftc -sdk `xcode-select -p`/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/ \
   -I./Sources/CXX/include \
   -Xfrontend -enable-cxx-interop \
   -Xfrontend -validate-tbd-against-ir=none \
