75e3d1e54767ca9dfc6d337c531a5a5d484f1536
Puyan Lotfi <puyan@puyan.org>
2021-07-30 20:24:55 -0400

    Removing non-template related stuff. Cleanup.

--- Sources/CXX/include/CXX.h
+++ Sources/CXX/include/CXX.h
@@ -2,25 +2,9 @@
 #define CXX_H
 
 #include <vector>
-#include <string_view>
-
-bool is_str_42(std::string_view view) {
-    return view == "42";
-}
-
-int cxx_f(int);
-
-struct MyStruct {
-  const int &operator[](int x) const { return 42; }
-};
-
-struct MyStructValType {
-  const int operator[](int x) const { return 42; }
-};
-
 struct V {
   std::vector<int> &getVec() { return Vec; }
-  std::vector<int> Vec = {1, 2, 3};
+  std::vector<int> Vec = {1, 2, 3, 4};
 };
 
 using cxx_std_vector_of_int = std::vector<int>;
