ccb1132d8bc9d8e6207f31408c20388cb860ceb6
Puyan Lotfi <puyan@puyan.org>
2021-08-02 18:24:40 -0400

    Dropping the extension to make the example cleaner.

--- Sources/Swift/main.swift
+++ Sources/Swift/main.swift
@@ -1,19 +1,13 @@
 import CXX
 import std.vector
 
-extension cxx_std_vector_of_int {
-  mutating func swift_array() -> Array<Self.value_type> {
-    let n = self.size() * MemoryLayout<Self.value_type>.stride
-    return Array<Self.value_type>(unsafeUninitializedCapacity: self.size()) {
-      _ = memcpy($0.baseAddress, self.data(), n)
-      $1 = $0.count
-    }
-  }
-}
-
 var v = V()
 var Vector : cxx_std_vector_of_int = v.getVec()[0]
-let arr = Vector.swift_array()
 
-print("std::vector: \(arr)")
+print("std::vector: ", terminator: "[ ")
+for i in 0..<Vector.size() {
+    print(Vector[i], terminator: " ")
+}
+print("]\n")
+
 print("Done.")
