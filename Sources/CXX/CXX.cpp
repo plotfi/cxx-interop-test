84e9e8dcc1436e2a1e199b80cd95036d2a7df01e
Puyan Lotfi <puyan@puyan.org>
2021-10-27 19:32:35 -0400

    Providing C++ support code (needed for now)
    Until this PR lands: https://github.com/apple/swift/pull/39551
    
    this support code is needed.

--- Sources/CXX/CXX.cpp
+++ Sources/CXX/CXX.cpp
@@ -1,6 +1,18 @@
 // TODO: Generate these for specializing COMDATs during irgen.
 #include <vector>
+#include <CXX.h>
 
-#if !DISABLE_TEMPLATE_SPECIALIZATION
 template class std::vector<int>;
-#endif
+template class std::__vector_base<int, std::allocator<int>>;
+template class std::__vector_base_common<true>;
+
+std::vector<int> a = {1, 2, 3};
+int f(int i) {
+  return a[i];
+}
+
+int g() {
+  V v;
+  auto vec = v.getVec();
+  return vec[0];
+}
