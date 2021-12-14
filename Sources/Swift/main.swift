9217fc40b32cda5de65be89db15d7e85b81080a8
Puyan Lotfi <puyan@puyan.org>
2021-12-14 18:10:42 -0500

    Adding a test case to show how copy on write semantics works with std::vector.
    Output should be:
    
    Initializing Vector1 as [ 1 2 3 4 5 ]
    Printing Vector1:
    std::vector: [ 1 2 3 4 5 ]
    
    Assigning Vector1 to Vector2
    Clearing Vector2 (std::vector<T>::clear()
    Printing Vector2:
    std::vector: [ ]
    
    Printing Vector1:
    std::vector: [ 1 2 3 4 5 ]
    
    Done.

--- Sources/Swift/main.swift
+++ Sources/Swift/main.swift
@@ -1,13 +1,40 @@
 import CXX
 import std.vector
 
-var v = V()
-var Vector = v.Vec
+typealias cxx_std_vector_of_int = std.__1.__CxxTemplateInstNSt3__16vectorIiNS_9allocatorIiEEEE
 
-print("std::vector: ", terminator: "[ ")
-for i in 0..<Vector.size() {
-    print(Vector[i], terminator: " ")
+func PrintVector(_ V: inout cxx_std_vector_of_int) {
+  print("std::vector: ", terminator: "[ ")
+  for i in 0..<V.size() {
+    print(V[i], terminator: " ")
+  }
+  print("]\n")
 }
-print("]\n")
+
+extension cxx_std_vector_of_int {
+  mutating func print() {
+    PrintVector(&self)
+  }
+}
+
+var v = V()
+
+print("Initializing Vector1 as [ 1 2 3 4 5 ]")
+var Vector1 = v.Vec
+
+print("Printing Vector1:")
+Vector1.print()
+
+print("Assigning Vector1 to Vector2")
+var Vector2 = Vector1
+
+print("Clearing Vector2 (std::vector<T>::clear()")
+Vector2.clear()
+
+print("Printing Vector2:")
+Vector2.print()
+
+print("Printing Vector1:")
+Vector1.print()
 
 print("Done.")
