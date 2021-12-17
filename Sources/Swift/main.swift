e24365353fcbec74777c5c372605dacde336c174
Puyan Lotfi <puyan@puyan.org>
2021-12-16 20:08:56 -0500

    Adding use of Generics syntax for td::vectors. Going to work on push_back next.

--- Sources/Swift/main.swift
+++ Sources/Swift/main.swift
@@ -1,9 +1,9 @@
 import CXX
 import std.vector
 
-typealias cxx_std_vector_of_int = std.__1.__CxxTemplateInstNSt3__16vectorIiNS_9allocatorIiEEEE
+typealias cxx_std_vector_int = std.vector<CInt, std.allocator<CInt>>
 
-func PrintVector(_ V: inout cxx_std_vector_of_int) {
+func PrintStdVector(_ V : inout std.vector<CInt, std.allocator<CInt>>) {
   print("std::vector: ", terminator: "[ ")
   for i in 0..<V.size() {
     print(V[i], terminator: " ")
@@ -11,10 +11,15 @@ func PrintVector(_ V: inout cxx_std_vector_of_int) {
   print("]\n")
 }
 
-extension cxx_std_vector_of_int {
-  mutating func print() {
-    PrintVector(&self)
-  }
+func push_back(_ V : inout cxx_std_vector_int, _ Val : CInt) {
+  let ValuePointer = UnsafeMutablePointer<CInt>.allocate(capacity: 1)
+
+  var Value: [CInt] = [Val]
+  defer { ValuePointer.deallocate() }
+  ValuePointer.initialize(from: &Value, count: 1)
+
+  // Link Error:
+  // V.push_back(ValuePointer)
 }
 
 var v = V()
@@ -23,7 +28,7 @@ print("Initializing Vector1 as [ 1 2 3 4 5 ]")
 var Vector1 = v.Vec
 
 print("Printing Vector1:")
-Vector1.print()
+PrintStdVector(&Vector1)
 
 print("Assigning Vector1 to Vector2")
 var Vector2 = Vector1
@@ -32,9 +37,12 @@ print("Clearing Vector2 (std::vector<T>::clear()")
 Vector2.clear()
 
 print("Printing Vector2:")
-Vector2.print()
+PrintStdVector(&Vector2)
 
 print("Printing Vector1:")
-Vector1.print()
+PrintStdVector(&Vector1)
+
+var Vector3 = std.vector<CInt, std.allocator<CInt>>()
+push_back(&Vector3, 42)
 
 print("Done.")
