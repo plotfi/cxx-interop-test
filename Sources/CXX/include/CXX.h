734ce476419a9777e07b3a67921aa1392e83cd80
Puyan Lotfi <puyan@puyan.org>
2022-06-25 22:03:18 -0400

    Block copy update

--- Sources/CXX/include/CXX.h
+++ Sources/CXX/include/CXX.h
@@ -5,49 +5,27 @@ NS_ASSUME_NONNULL_BEGIN
 typedef NSString *_Nonnull (^UITextBlock)();
 
 struct UITextAttribute {
-  UITextAttribute() = default;
-  UITextAttribute(UITextBlock _Nullable textBlock): textBlock(textBlock) {};
   UITextAttribute(NSString *_Nullable text):
     textBlock(text ? ^{ return text; } : (UITextBlock)nil) {};
 
   UITextBlock _Nullable textBlock;
 };
 
-
 struct UIAccessibilityContext {
   NSString *_Nullable accessibilityIdentifier;
-  NSNumber *_Nullable isAccessibilityElement;
   UITextAttribute accessibilityLabel;
-  UITextAttribute accessibilityHint;
-  UITextAttribute accessibilityValue;
-  /**
-   Arbitrary extra data about accessibility. ComponentKit ignores this data,
-   but you may use it for accessibility-related abstractions.
-   */
   NSDictionary<NSString *, id> *_Nullable extra;
 
-  /**
-   Unconventional, but there's an issue with aggregate initializers and Swift interop.
-   Adding a user-defined ctor would disable the aggregate init - which we don't want,
-   so using a static method for now.
-  */
-  static UIAccessibilityContext build(NSString *_Nullable accessibilityIdentifier,
-                                      NSNumber *_Nullable isAccessibilityElement,
-                                      UITextAttribute accessibilityLabel,
-                                      UITextAttribute accessibilityHint,
-                                      UITextAttribute accessibilityValue,
-                                      NSDictionary<NSString *, id> *_Nullable extra)
-  {
+  static UIAccessibilityContext
+  build(NSString *_Nullable accessibilityIdentifier,
+        UITextAttribute accessibilityLabel,
+        NSDictionary<NSString *, id> *_Nullable extra) {
     return {
-      .accessibilityIdentifier = accessibilityIdentifier,
-      .isAccessibilityElement = isAccessibilityElement,
-      .accessibilityLabel = accessibilityLabel,
-      .accessibilityHint = accessibilityHint,
-      .accessibilityValue = accessibilityValue,
-      .extra = extra,
+        .accessibilityIdentifier = accessibilityIdentifier,
+        .accessibilityLabel = accessibilityLabel,
+        .extra = extra,
     };
   }
 };
-
 
 NS_ASSUME_NONNULL_END
--- Sources/Swift/main.swift
+++ Sources/Swift/main.swift
@@ -1,37 +1,12 @@
 import CXX
 import Foundation
 
-public struct AccessibilityContext {
-  public var accessibilityIdentifier: String?
-  public var isAccessibilityElement: Bool?
-  public var accessibilityLabel: String?
-  public var accessibilityHint: String?
-  public var accessibilityValue: String?
-
-  public init(
-    accessibilityIdentifier: String? = nil,
-    isAccessibilityElement: Bool? = nil,
-    accessibilityLabel: String? = nil,
-    accessibilityHint: String? = nil,
-    accessibilityValue: String? = nil) {
-      self.accessibilityIdentifier = accessibilityIdentifier
-      self.isAccessibilityElement = isAccessibilityElement
-      self.accessibilityLabel = accessibilityLabel
-      self.accessibilityHint = accessibilityHint
-      self.accessibilityValue = accessibilityValue
-    }
-}
-
-let accessibilityContext = AccessibilityContext(
-                            accessibilityLabel: "Cancel"
-                          )
+let accessibilityIdentifier = "Hello"
+let accessibilityLabel = "Cancel"
 
 _ = UIAccessibilityContext.build(
-  accessibilityContext.accessibilityIdentifier,
-  accessibilityContext.isAccessibilityElement as NSNumber?,
-  UITextAttribute(accessibilityContext.accessibilityLabel), // crash
-  UITextAttribute(accessibilityContext.accessibilityHint),
-  UITextAttribute(accessibilityContext.accessibilityValue),
+  accessibilityIdentifier,
+  UITextAttribute(accessibilityLabel), // crash
   nil
 )
 
