5085c2c7e99b07431398e1beb1f689609fa8ef61
Puyan Lotfi <plotfi@meta.com>
2023-11-05 01:23:13 -0400

    virt function test

--- Sources/CXX/CXX.cpp
+++ Sources/CXX/CXX.cpp
@@ -1,17 +1,2 @@
 // TODO: Generate these for specializing COMDATs during irgen.
-#include <vector>
 
-#if !(BOOTSTRAPINTEROP)
-#include "swift-header.h"
-#endif
-
-template class std::vector<int>;
-
-int TheAnswersAnswer() {
-  #if !(BOOTSTRAPINTEROP)
-  return main::TheAnswer() + main::TheAnswer();
-  #else
-  exit(EXIT_FAILURE);
-  return 0;
-  #endif
-}
--- Sources/CXX/include/CXX.h
+++ Sources/CXX/include/CXX.h
@@ -1,27 +1,11 @@
 #ifndef CXX_H
 #define CXX_H
 
-#include <vector>
-#include <string_view>
+struct S {
+  int a;
+  [[nodiscard]] auto getA() const -> int { return a; }
 
-bool is_str_42(std::string_view view) {
-    return view == "42";
-}
-
-namespace NS1 {
-  enum class Color {
-    White,
-    Black,
-    Green,
-    Red,
-  };
-}
-
-int TheAnswersAnswer();
-
-using cxx_std_vector_of_int = std::vector<int>;
-
-#include <functional>
-std::function<int(int)> getFunction() { return [](int a) { return 0; }; }
+  virtual int f() { return a; }
+};
 
 #endif
